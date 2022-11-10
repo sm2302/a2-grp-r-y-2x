f19e97383e794fa7adbd9819f088ed3653071231
AiniAbd <111748570+AiniAbd@users.noreply.github.com>
2022-11-10 11:55:43 +0800

    Method A draft
    Upload Method A draft

--- solutions.R
+++ solutions.R
@@ -16,19 +16,37 @@ eqtri_df <- tibble(
 )
 
 # Coordinates of random chord
-rdmchr_df <- tibble(
-  x    = 0.93636368,
-  y    = 0.35103142,
-  xend = -0.9999991,
-  yend = -0.001326758
+x0 = 0;
+y0 = 0;
+r = 1;
+n = 10;
+
+# Method A
+
+angle_ep1 = 2*pi*runif(n);
+angle_ep2 = 2*pi*runif(n);
+
+# Endpoint coordinates
+x1 = x0 + r*cos(angle_ep1);
+y1 = y0 + r*cos(angle_ep1);
+
+x2 = x0 + r*cos(angle_ep2);
+y2 = y0 + r*cos(angle_ep2);
+
+
+MethodA_df <- tibble(
+  x    = x1,
+  y    = y1,
+  xend = x2,
+  yend = y2
 )
 
 # Plot
 p <- ggplot() +
   ggforce::geom_circle(aes(x0 = 0, y0 = 0, r = 1), col = "gray50") +
   geom_segment(data = eqtri_df, aes(x = x, y = y, xend = xend, yend = yend)) +
-  geom_segment(data = rdmchr_df, aes(x = x, y = y, xend = xend, yend = yend),
+  geom_segment(data = MethodA_df, aes(x = x, y = y, xend = xend, yend = yend),
                col = "red3") +
   coord_equal()
 
-ggsave(p, file = "plot.png", height = 5, width = 7)
+#ggsave(p, file = "plot.png", height = 5, width = 7)
