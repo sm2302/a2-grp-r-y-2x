2743790d6307da83fd7a4107d69d046b666bb51b
AinaRapawi <19b2130@ubd.edu.bn>
2022-11-10 14:27:42 +0800

    Update solutions.R
    Upload draft Method C

--- solutions.R
+++ solutions.R
@@ -16,22 +16,22 @@ eqtri_df <- tibble(
 )
 
 # Coordinates of random chord
-x0 = 0;
-y0 = 0;
-r = 1;
-n = 10;
+x0 = 0
+y0 = 0
+r = 1
+n = 10
 
 # Method A
 
-angle_ep1 = 2*pi*runif(n);
-angle_ep2 = 2*pi*runif(n);
+angle_ep1 = 2*pi*runif(n)
+angle_ep2 = 2*pi*runif(n)
 
 # Endpoint coordinates
-x1 = x0 + r*cos(angle_ep1);
-y1 = y0 + r*sin(angle_ep1);
+x1 = x0 + r*cos(angle_ep1)
+y1 = y0 + r*sin(angle_ep1)
 
-x2 = x0 + r*cos(angle_ep2);
-y2 = y0 + r*sin(angle_ep2);
+x2 = x0 + r*cos(angle_ep2)
+y2 = y0 + r*sin(angle_ep2)
 
 
 MethodA_df <- tibble(
@@ -42,10 +42,41 @@ MethodA_df <- tibble(
 )
 
 # Plot
-p <- ggplot() +
+p1 <- ggplot() +
   ggforce::geom_circle(aes(x0 = 0, y0 = 0, r = 1), col = "gray50") +
   geom_segment(data = eqtri_df, aes(x = x, y = y, xend = xend, yend = yend)) +
   geom_segment(data = MethodA_df, aes(x = x, y = y, xend = xend, yend = yend), col = "red") +
   coord_equal()
 
-#ggsave(p, file = "plot.png", height = 5, width = 7)
+
+# Method C
+
+angle_mdpt = 2*pi*runif(n)
+a = r*sqrt(runif(n))
+d = sqrt(r^2 - a^2)
+
+# Endpoint coordinates
+x5 = x0 + a*cos(angle_mdpt) + d*sin(angle_mdpt)
+y5 = x0 + a*sin(angle_mdpt) - d*cos(angle_mdpt)
+x6 = x0 + a*cos(angle_mdpt) - d*sin(angle_mdpt)
+y6 = x0 + a*sin(angle_mdpt) + d*cos(angle_mdpt)
+
+# Midpoint of segments
+mdptx = (x5 + x6) / 2
+mdpty = (y5 + y6) / 2
+
+MethodC_df <- tibble(
+  mdptx = mdptx,
+  mdpty = mdpty,
+  x    = x5,
+  y    = y5,
+  xend = x6,
+  yend = y6
+)
+
+p3 <- ggplot() +
+  ggforce::geom_circle(aes(x0 = 0, y0 = 0, r = 1), col = "gray50") +
+  geom_segment(data = eqtri_df, aes(x = x, y = y, xend = xend, yend = yend)) +
+  geom_point(data = MethodC_df, aes(x = mdptx, y = mdpty)) +
+  geom_segment(data = MethodC_df, aes(x = x, y = y, xend = xend, yend = yend), col = "blue") +
+  coord_equal()
