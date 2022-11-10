f6da5542c964b82451d00d35b32f9e2ed371a298
AinaRapawi <19b2130@ubd.edu.bn>
2022-11-10 12:10:36 +0800

    Upload Method A
    Upload draft Method A

--- solutions.R
+++ solutions.R
@@ -3,7 +3,7 @@
 
 library(tidyverse)
 library(ggforce)
-theme_set(theme_void())
+
 
 # Draw a random chord in a unit circle centred at origin -----------------------
 
@@ -28,10 +28,10 @@ angle_ep2 = 2*pi*runif(n);
 
 # Endpoint coordinates
 x1 = x0 + r*cos(angle_ep1);
-y1 = y0 + r*cos(angle_ep1);
+y1 = y0 + r*sin(angle_ep1);
 
 x2 = x0 + r*cos(angle_ep2);
-y2 = y0 + r*cos(angle_ep2);
+y2 = y0 + r*sin(angle_ep2);
 
 
 MethodA_df <- tibble(
@@ -45,8 +45,7 @@ MethodA_df <- tibble(
 p <- ggplot() +
   ggforce::geom_circle(aes(x0 = 0, y0 = 0, r = 1), col = "gray50") +
   geom_segment(data = eqtri_df, aes(x = x, y = y, xend = xend, yend = yend)) +
-  geom_segment(data = MethodA_df, aes(x = x, y = y, xend = xend, yend = yend),
-               col = "red3") +
+  geom_segment(data = MethodA_df, aes(x = x, y = y, xend = xend, yend = yend), col = "red") +
   coord_equal()
 
 #ggsave(p, file = "plot.png", height = 5, width = 7)
